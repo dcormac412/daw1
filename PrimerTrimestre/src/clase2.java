84f5724ed6ec218bf2db56ac242a89557ffdd062
Daniel Cortés <danielcortesmacias@gmail.com>
2023-09-23 12:06:32 +0200

    realice cambios

---
+++ PrimerTrimestre/src/clase2.java
@@ -0,0 +1,36 @@
+
+public class clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double d1 = 10;
+		double d2 = 20;
+		
+		System.out.println("1. El resultado de restar 4 al doble de d1 es : " + ((d1*2)-4));
+		
+		System.out.println("2. El resultado de restar 4 a d1 y calcular el doble es : " + ((d1-4)*2));
+		
+		System.out.println("3. El resultado de restar 2 a d1 y dividirlo por 12 y a todo ello sumarle d2 es : " + (d1+2)/12 +d2);
+		
+		System.out.println("4. El resultado de dividir d2 entre d1 y todo ello dividirlo entre 2 es : " +(d2/d1)/2);
+		
+		System.out.println("5. El resultado de dividir d2 entre la mitad de d1 es : " + d2/(d1/2));
+		
+		System.out.println("6. El resultado de restar d2 a un cuarto de d1 es : " + (d2-(d1/3)));
+		
+		System.out.println("7. El resultado de dividir d1 entre d2 y todo ello dividirlo entre 4 es : " +(d1-d2)/4);
+		
+		System.out.println("8. El resultado de dividir d2 entre d1 y todo ello multiplicarlo por 2 es : " +(d2/d1)*2);
+		
+		System.out.println("9. El resultado de dividir d2 entre el doble de d1 es : " +(d2/(d1*2)));
+		
+		System.out.println("10. El resultado de restar el doble de d2 a un cuarto de d1 es : " + (d2*2-d1/3));
+		
+		System.out.println("11. El resultado de multiplicar d2 por 100 y restarle d1 es: " + (d2*100-d1));
+		System.out.println("12. El resultado de multiplicar d2 por 50 más d1 y todo ello dividirlo entre 10 es : " +(d2*50+d1)/10);
+		
+
+	}
+
+}
--- PrimerTrimestre/src/probando.java
+++ PrimerTrimestre/src/probando.java
@@ -2,9 +2,31 @@
 public class probando {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Estoy probando");
+		// Creacion de las variables
+		
+		int i;
+		i=10;
+		
+		double d = 3.14;
+		
+		char c1 = 'a';
+		
+		char c2 = 'A';
+		
+		boolean encontrado = true;
+		
+		String mensaje = "Bienvenido a Java";
+		
+		//Mostrar en consola
+		System.out.println("La variable i es de tipo entero y su valor es : " + i);
+		System.out.println("La variable d es de tipo double y su valor es : " + d);
+		System.out.println("La variable c1 es de tipo double y su valor es : " + c1);
+		System.out.println("La variable c2 es de tipo double y su valor es : " + c2);
+		System.out.println("La variable encontrado es de tipo double y su valor es : " + encontrado);
+		System.out.println("La variable mensaje es de tipo double y su valor es : " + mensaje);
+		
 
 	}
-
-}
+	
+	
+	}
